49105815ba82bfad8fb044e912b8eb580558bcf2
Panos <panossmav@icloud.com>
2025-08-09 18:55:34 +0300

    Refactor vehicle and secondary home creation functions for improved clarity

--- assets/scripts/tekmirio-diaviosis.js
+++ assets/scripts/tekmirio-diaviosis.js
@@ -1,6 +1,8 @@
 
 function Calculate(){
     const first_home = CalculateFirstHome()
+    const second_homes = CalculateSecondaryHomes()
+    const vehicles = CalculateVehicles()
     //Final function pending calculations
     
 }
@@ -22,6 +24,75 @@ function CalculateFirstHome(){
     final_price = Math.round(final_price*100)/100
     return final_price;
 }
+
+function CalculateSecondaryHomes(){
+    let secondary_homes_price = 0;
+    const properties = document.getElementsByName("sec_homes_sqf");
+    for (let i = 0;i < properties.length;i++) {
+        let property_price = 0;
+        let x = Number(properties[i].value);
+        if (x<=80){
+            property_price = 40*x;
+        }else if (x<=120) {
+            property_price = 65*x;
+        }else if (x<=200) {
+            property_price = 110*x;
+        }else if (x<=300){
+            property_price = 200*x;
+        }else {
+            property_price = 400*x;
+        }
+        property_price = property_price * 0.5;
+        secondary_homes_price+=property_price;
+    }
+    return secondary_homes_price;
+}
+
+function CalculateVehicles(){
+    let cc_list = document.getElementsByName("car-cc");
+    let year_list = document.getElementsByName("car-year");
+    const current_year = new Date().getFullYear();
+    let total_cost_all = 0;
+    for (let i = 0;i < cc_list.length;i++){
+        let cc = Number(cc_list[i].value);
+        let year = Number(year_list[i].value);
+        let total_cost = 0;
+        if (cc<=1200){
+            total_cost = 4000;
+        } else if(cc<=1350){
+            total_cost = 5000;
+        } else if(cc<=1500){
+            total_cost = 5500;
+        } else if(cc<=1600){
+            total_cost= 6000;
+        } else if(cc<=1800){
+            total_cost = 7000;
+        } else if(cc<=2000){
+            total_cost = 8500;
+        } else if(cc<=2500){
+            total_cost = 11500;
+        }else{
+            const remaining_cc = Math.round((cc-2500)/100);
+            total_cost = 11500+remaining_cc;
+
+        }
+        const age= current_year - year;
+        if (age<=5){
+            total_cost = total_cost;
+        } else if(age<=10){
+            total_cost = total_cost*0.7;
+        } else if(age<=15){
+            total_cost = total_cost*0.5;
+        } else{
+            total_cost = total_cost*0.25;
+        }
+
+        total_cost = Math.round(total_cost*100)/100;
+        total_cost_all+=total_cost;
+    }
+    return total_cost_all;
+}
+
 function CreateSecondaryHome(){
     document.getElementById("optional-secondary-properties").textContent='';
     let new_home = document.createElement("div");
